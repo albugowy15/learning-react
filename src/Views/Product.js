8375c6afd9f95d56f9eddbb486748e89ed996ca3
albugowy15 <kholidbughowi@gmail.com>
2023-01-02 21:20:16 +0700

    add loading and error state

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,34 +1,62 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
 
 function Product() {
   const { id } = useParams()
   const url = `https://63b29f845e490925c51cff54.mockapi.io/products/${id}`
-  const [product, setProduct] = useState(null)
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false
+  })
 
   let content = null
 
   useEffect(() => {
+    setProduct({
+      loading: true,
+      data: null,
+      error: false
+    })
     axios.get(url).then(response => {
-      setProduct(response.data)
+      setProduct({
+        loading: false,
+        data: response.data,
+        error: false
+      })
+    }).catch(() => {
+      setProduct({
+        loading: false,
+        data: null,
+        error: true
+      })
     })
   }, [url])
 
-  if (product) {
+  if (product.error) {
+    content = <p>There was an error please refresh or try again later</p>
+  }
+
+  if (product.loading) {
+    content = <Loader />
+  }
+
+  if (product.data) {
     content =
       <div>
-        <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+        <h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
 
         <div>
-          <img width={300} src={product.images[0].imageUrl} alt={product.name}></img>
+          <img width={300} src={product.data.images[0].imageUrl} alt={product.data.name}></img>
         </div>
 
         <div className="font-bold text-xl mb-3">
-          $ {product.price}
+          $ {product.data.price}
         </div>
         <div>
-          {product.description}
+          {product.data.description}
         </div>
       </div>
 
