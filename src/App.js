191102b4ea3babc250780d88c8f19a8d07ca2ff9
albugowy15 <kholidbughowi@gmail.com>
2023-01-02 16:03:33 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
-import CounterExample from './Components/CounterExample';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
-import HelloWorld from './Components/HelloWorld';
+import About from './Views/About';
+import Home from './Views/Home';
 
 function App() {
   return (
     <div>
-      <Header />
-      <HelloWorld name="Bughowi" />
-      <Footer />
+      <Router>
+        <Header />
+
+        <div className='p-3'>
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+
+            <Route path="/about" element={<About />} />
+
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
       {/* <CounterExample /> */}
     </div>
   );
