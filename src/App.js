e4c6f3a5a5b094e96ab5aad02ddcd371a0e43f2b
albugowy15 <kholidbughowi@gmail.com>
2023-01-02 14:48:25 +0700

    create a counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CounterExample from './Components/CounterExample';
 import HelloWorld from './Components/HelloWorld';
 
 function App() {
   return (
     <div>
-      <HelloWorld name="Bughowi" />
+      {/* <HelloWorld name="Bughowi" /> */}
+      <CounterExample />
     </div>
   );
 }
