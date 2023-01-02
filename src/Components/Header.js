21bafb6466158b5da45c09946b36bdfcd36dffd4
albugowy15 <kholidbughowi@gmail.com>
2023-01-02 15:19:14 +0700

    create navigation component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <header className="font-bold border-b p-3">AppName</header>
+    <header className="border-b p-3 flex justify-between items-center">
+      <span className="font-bold">AppName</span>
+      <Navigation />
+    </header>
   )
 }
 
